0290021eb4b6d108e919eb01efbafeb276d47bfc
Noh MoonJin <neowiztomato@neowiz.com>
2018-01-22 20:10:44 +0900

    make skeleton structure

---
+++ src/java8/book/chapter1.java
@@ -0,0 +1,66 @@
+package java8.book;
+
+/**
+ * Created by neowiztomato on 2018-01-22.
+ */
+public class chapter1 {
+    public static void main(String... argc) {
+        System.out.println("===== chapter1 ==========");
+        exercise1();
+        exercise2();
+        exercise3();
+        exercise4();
+        exercise5();
+        exercise6();
+        exercise7();
+        exercise8();
+    }
+
+    public static void exercise1() {
+        // 01. Arrays.sort 메서드에서 비교자 코드는 sort 호출과 같은 스레드에서 호출되는가, 다른 스레드에서 호출되는가?
+        System.out.println("===== exercise 1 =====");
+
+    }
+
+    public static void exercise2() {
+        // 02. java.io.File 클래스의 listFiles(FileFilter)와 isDirectory 메서드를 이용해 주어진 디렉터리의
+        // 모든 서브디렉터리를 리턴하는 메서드를 작성하라. FileFilter 객체 대신 람다 표현식을 사용하라.
+        // 메서드 표현식을 이용해 같은 작업을 반복하라.
+        System.out.println("===== exercise 2 =====");
+
+    }
+
+    public static void exercise3() {
+        // 03. java.io.File 클래스의 list(FilenameFilter) 메서드를 이용해 주어진 디렉터리에서 주어진 확장자를
+        // 지닌 모든 파일을 리턴하는 메서드를 작성하라. FilenameFilter가 아닌 람다 표현식을 사용하라.
+        // 이 람다 표현식을 자신이 감싸는 유효 범위에 있는
+        System.out.println("===== exercise 3 =====");
+
+    }
+
+    public static void exercise4() {
+        System.out.println("===== exercise 4 =====");
+
+    }
+
+    public static void exercise5() {
+        System.out.println("===== exercise 5 =====");
+
+    }
+
+    public static void exercise6() {
+        System.out.println("===== exercise 6 =====");
+
+    }
+
+    public static void exercise7() {
+        System.out.println("===== exercise 7 =====");
+
+    }
+
+    public static void exercise8() {
+        System.out.println("===== exercise 8 =====");
+
+    }
+
+}

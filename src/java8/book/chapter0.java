b6f033388db1924633f561cbe986eba3bf21bd1b
Noh MoonJin <neowiztomato@neowiz.com>
2018-01-22 20:09:34 +0900

    개발환경을 intellij로 변경

---
+++ src/java8/book/chapter0.java
@@ -0,0 +1,77 @@
+package java8.book;
+
+/**
+ 안녕하세요-
+
+ 설문조사 결과 매우 다양하게 시간이 분포되었습니다.
+ 월 4-6시 2표
+ 월 오전 1표
+ 수 오전 1표
+ 수 6시 이후 1표
+ 금 4-6시 1표
+
+ 첫 스터디는 1월 29일(월) 4시에 진행합니다. 회의실 잡기가 무지 어렵네요..;;
+ 스터디 시간 전까지 1챕터를 읽으시고, 1챕터 연습 문제 중 풀고 싶은 것 2개를 골라 Git에 올려주세요.
+ 연습 문제는 겹치는 것 상관없습니다.
+
+ 강의 자료는 따로 만들지 않고 스터디 중 논의된 내용은 위키에 회의록처럼 정리하도록 하겠습니다.
+
+ 감사합니다.
+
+ *
+ * Created by neowiztomato on 2018-01-22.
+ */
+public class chapter0 {
+    public static void main(String... argc) {
+        System.out.println("== chapter 0 ==========");
+        exercise1();
+        exercise2();
+        exercise3();
+        exercise4();
+        exercise5();
+        exercise6();
+        exercise7();
+        exercise8();
+    }
+
+    public static void exercise1() {
+        System.out.println("===== exercise 1 =====");
+
+    }
+
+    public static void exercise2() {
+        System.out.println("===== exercise 2 =====");
+
+    }
+
+    public static void exercise3() {
+        System.out.println("===== exercise 3 =====");
+
+    }
+
+    public static void exercise4() {
+        System.out.println("===== exercise 4 =====");
+
+    }
+
+    public static void exercise5() {
+        System.out.println("===== exercise 5 =====");
+
+    }
+
+    public static void exercise6() {
+        System.out.println("===== exercise 6 =====");
+
+    }
+
+    public static void exercise7() {
+        System.out.println("===== exercise 7 =====");
+
+    }
+
+    public static void exercise8() {
+        System.out.println("===== exercise 8 =====");
+
+    }
+
+}
